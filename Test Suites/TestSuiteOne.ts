c480c749e2c94eca06545e6af5366d6b557d7c32
Testbits-Pami <fahmi@mytestbits.com>
2022-11-11 17:42:35 +0800

    commit

--- Test Suites/TestSuiteOne.ts
+++ Test Suites/TestSuiteOne.ts
@@ -5,9 +5,9 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>3</numberOfRerun>
-   <pageLoadTimeout>30</pageLoadTimeout>
-   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>7</pageLoadTimeout>
+   <pageLoadTimeoutDefault>false</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>a696ac5b-8a69-4182-8866-37e7c3dd56b9</testSuiteGuid>
