7abc519e9a317509ad23cb22c5322f4358278a0e
Testbits-Pami <fahmi@mytestbits.com>
2022-11-15 15:41:44 +0800

    update

--- Test Suites/TestSuiteOne.ts
+++ Test Suites/TestSuiteOne.ts
@@ -14,15 +14,8 @@
    <testCaseLink>
       <guid>d4391c2a-4561-4b7a-ada6-b4662360a978</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/Login</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>d701197f-5bc8-4f24-bf1d-308cd176ab6c</guid>
-      <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/TEST-1 TC1-Successfully Login to OrangeHRM</testCaseId>
+      <testCaseId>Test Cases/TEST-1 Login</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
